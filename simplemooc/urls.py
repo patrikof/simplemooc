393b8189d794f3769a023fdb01d4518147d3cfa4
Patrik de Oliveira França <patrikoliveira@yahoo.com.br>
2020-03-09 00:06:27 -0300

    manutenção urls

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [	
-	path('core/', include('simplemooc.core.urls')),
+	path('', include('simplemooc.core.urls')),
     path('admin/', admin.site.urls),
 ]
